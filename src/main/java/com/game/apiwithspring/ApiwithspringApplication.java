8bb7797434ec5ef503079b512ef602ae4a63324a
Spirit <dev.gounaya@gmail.com>
2020-11-07 23:05:31 +0100

    init project test

---
+++ src/main/java/com/game/apiwithspring/ApiwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.game.apiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwithspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/apiwithspring/ApiwithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.apiwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
